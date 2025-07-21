c0ad983269430fb4c57d5c9b50f2da182dc0411f
Lizardo-Castillo <lcastillol@unsa.edu.pe>
2025-07-20 21:51:50 -0500

    Conexion con la base de datos PostgreSQL

--- src/main/java/com/backend/backend/domain/repository/CitaRepository.java
+++ src/main/java/com/backend/backend/domain/repository/CitaRepository.java
@@ -1,5 +1,8 @@
 package com.backend.backend.domain.repository;
+import com.backend.backend.domain.model.Cita;
+import org.springframework.data.jpa.repository.JpaRepository;
 
-public class CitaRepository {
+public interface CitaRepository extends JpaRepository<Cita, Long> {
+    // Define any custom query methods if needed
 
 }
